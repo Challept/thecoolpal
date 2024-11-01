1e2a5a6d0169d08adb193d098284f990e90c6799
Notch3w <119951713+Challept@users.noreply.github.com>
2024-11-01 23:02:30 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,20 +1,18 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
+document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault();
-
+    
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const phone = document.getElementById('phone').value.trim();
     const errorMessage = document.getElementById('error-message');
-
-    // Anropa Google Apps Script för att hämta data från Google Sheets
-    fetch('https://script.google.com/macros/s/AKfycbxm783fL77u2c1jOqmTGDY2wJzuzcCexPkLvYQHB1NiyVxiV-dFhoNWbuuDrmFYt47sXg/exec')
+    
+    fetch('https://script.google.com/macros/s/AKfycbzNeQrY2s4igGV-eY0EHE8hAV4B7nImhpETsNP8w0y-F2rjA45BmijnAkTmBYu60xXOhw/exec')
         .then(response => response.json())
         .then(data => {
             const user = data.find(user => user.name === name && user.email === email && user.phone === phone);
 
             if (user) {
                 alert('Inloggning lyckades!');
-                // Dirigera användaren till adminpanelen eller dashboard-sidan
                 window.location.href = 'dashboard.html';
             } else {
                 errorMessage.style.display = 'block';
@@ -22,7 +20,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
             }
         })
         .catch(error => {
-            console.error('Fel vid inläsning av data:', error);
+            console.error('Error fetching data:', error);
             errorMessage.style.display = 'block';
             errorMessage.innerText = 'Ett tekniskt fel uppstod. Vänligen försök igen senare.';
         });
